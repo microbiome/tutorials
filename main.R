9bdd06b2e5f01ccea2d841b2520ce9bfab2e658a
antagomir <leo.lahti@iki.fi>
2019-06-18 20:57:21 +0300

    homepage update

--- main.R
+++ main.R
@@ -1,61 +1,40 @@
-
-source('init.R')
+# source('init.R')
 library(microbiome)
 library(phyloseq)
-# library(netresponse)
+library(netresponse)
 library(MASS)
 library(dplyr)
 library(tidyr)
 library(ggplot2)
 library(devtools)
 library(ggplot2)
-
-#library(devtools)
-#load_all()
 library(microbiome)
-
 library(rmarkdown)
 library(knitr)
 library(knitcitations)
 
 # ---------------------------------------------
 
-library(knitr)
-#fs <- "Density.Rmd"
-#fs <- "Experimental.Rmd"
-#fs <- c("DMM.Rmd", "index.Rmd")
-#fs <- "Diversity.Rmd"
-#fs <- "Heatmap.Rmd"
-#fs <- "Profiling.Rmd"
-#fs <- "RDA.Rmd"
-#fs <- "SQL.Rmd"
-#fs <- "index.Rmd"
-#fs <- "rstanarm.Rmd"
-#fs <- "Landscaping.Rmd"
-#fs <- sample(list.files(pattern = ".Rmd$"), 20)
+# List the Rmd files to render
 fs <- sample(list.files(pattern = ".Rmd$")) # Random order
 
+# List rendering options
 knitr::opts_chunk$set(fig.path = "figure/", dev="CairoPNG")
-
 times <- c()
-namespaces0 <-  loadedNamespaces()
 
-#for (myfile in setdiff(fs, "Themes.Rmd")) {
-fix.these <- c("DMM.Rmd")
-for (myfile in setdiff(fs, fix.these)) {
+# Render all files
+for (myfile in fs) {
 
-    rmarkdown::render(myfile)
+    times[[myfile]] <- system.time(rmarkdown::render(myfile))
 
 }
 
-# Time per index.page
-# par(mar = c(3, 10, 1, 1)); barplot(sort(times), horiz = T, las = 1)
+rmarkdown::render("info.Rmd")
 
-#system("git add *.md")
-#system("git add figure/*")
-#system("git add *.Rmd")
+# git add + commit + push
+system("git add figure/*")
+system("git add *.Rmd")
 system("git add *.html")
 system("git commit -a -m'homepage update'")
 system("git push origin master")
-#system("rm -rf cache")
-#system("rm -rf figure")
+

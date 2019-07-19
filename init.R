5471cd395d26545bb585a4fd36b3070302c2e626
Wisam Tariq <wisam.tariqsaleem@utu.fi>
2019-07-19 13:38:50 +0300

    u

--- init.R
+++ init.R
@@ -1,17 +1,17 @@
+library(BiocManager)
 if (!requireNamespace("BiocManager", quietly = TRUE))
-  install.packages("BiocManager", "Biostyle")
+  install.packages("BiocManager")
 
 
 .cran_packages <- c("ggplot2", "gridExtra",'ape', 'dplyr','ggpubr', 'knitr','Cairo','devtools','data.table','splitstackshape', 'rmarkdown',
                     'tidyverse', 'readxl', 'vegan','knitcitations', 'reshape', 'reshape2',
                     'magrittr', 'vegan', 'IRanges','glue','stringr','devtools','captioner', 'rstan','rstanarm',
-                    'hrbrthemes', 'gcookbook','GGally', 'rvg','ggiraph','network',
-                    'ggnet','intergraph', 'rmdformats', 'FD', 'hrbrthemes')
+                    'hrbrthemes', 'gcookbook','GGally', 'rvg','ggiraph','network', 'gplots',
+                    'ggnet','intergraph', 'rmdformats', 'FD', 'hrbrthemes', 'GGally', 'lattice')
 
 .bioc_packages <- c("dada2", "phyloseq",'SummarizedExperiment','Biobase', 
-                    'BiocInstaller', "DECIPHER",'IRanges','BiocGenerics', "phangorn",
-                    'BiocStyle', "microbiome", "DESeq2", 'DirichletMultinomial',
-                    'netresponse')
+                   "DECIPHER",'IRanges','BiocGenerics', "phangorn",
+                    'BiocStyle', "microbiome", "DESeq2", 'DirichletMultinomial')
 
 # package.version("microbiome")
 #[1] "1.5.31"
@@ -26,12 +26,17 @@ install_github('briatte/ggnet')
 
 .inst <- .bioc_packages %in% installed.packages()
 if(any(!.inst)) {
-  source("http://bioconductor.org/biocLite.R")
+  #source("http://bioconductor.org/biocLite.R")
   
-  biocLite(.bioc_packages[!.inst], ask = F)
+  BiocManager::install(.bioc_packages[!.inst], ask = F)
 }
 install_github('antagomir/netresponse')
 install_github('microsud/microbiomeutilities')
+###
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
 
+BiocManager::install(c("SummarizedExperiment", 'DelayedArray'))
 # Load packages into session, and print package version
 sapply(c(.cran_packages, .bioc_packages, 'SpiecEasi', 'ggnet' ), require, character.only = TRUE)
+
